6989626f0514b8281b177f943a350b34bd4961a0
Lukas Koedijk <lukas@koedijk.net>
2018-09-06 14:50:50 +0200

    done 2-6, started 7

--- lab1/lukas/Lab1.hs
+++ lab1/lukas/Lab1.hs
@@ -41,17 +41,118 @@ eq1 = \xs -> left1 0 [0..xs] == right1 xs
 test1 = quickCheckResult (\n -> n >= 0 --> eq1 n)
 
 -- Workshop 3
-left2 :: Int -> [Int] -> Int
-left2 x [] = x
-left2 x (y:ys) = left2 (x + y^3) ys
+left1w3 :: Int -> [Int] -> Int
+left1w3 x [] = x
+left1w3 x (y:ys) = left1w3 (x + y^3) ys
+
+right1w3 :: Int -> Int
+right1w3 n = ((n*(n+1)) `div` 2)^2
+
+eq1w3 :: Int -> Bool
+eq1w3 = \xs -> left1w3 0 [0..xs] == right1w3 xs
+
+test1w3 = quickCheckResult (\n -> n >= 0 --> eq1w3 n)
+
+-- Exercise 2
+left2 :: Int -> Int
+left2 n = length (subsequences [1..n])
 
 right2 :: Int -> Int
-right2 n = ((n*(n+1)) `div` 2)^2
+right2 n = 2^n
 
 eq2 :: Int -> Bool
-eq2 = \xs -> left2 0 [0..xs] == right2 xs
+eq2 = \xs -> left2 xs == right2 xs
 
 test2 = quickCheckResult (\n -> n >= 0 --> eq2 n)
+
+-- It is hard to test because there are many subsequence that
+-- all have to be calculated. When performing this test we are actually
+-- testing if subsequences satify part of its specification.
+
+-- Exercise 3
+left3 :: Int -> Int
+left3 n = length (permutations [1..n])
+
+factorial :: Int -> [Int] -> Int
+factorial x [] = x
+factorial x (y:ys) = factorial (x*y) ys
+
+eq3 = \xs -> left3 xs == factorial 1 [1..xs]
+
+test3 = quickCheckResult (\n -> n >= 0 --> eq3 n)
+
+-- It is hard to test because there are many permutations that
+-- all have to be calculated. When performing this test we are actually
+-- testing if permutations satify part of its specification.
+
+-- Exercise 4
+reversalPrime :: [Integer] -> [Integer] -> [Integer]
+reversalPrime [] p = p
+reversalPrime (n:ns) p
+    | prime n && prime (reversal n) = reversalPrime ns (p ++ [n])
+    | otherwise = reversalPrime ns p
+
+test4 = reversalPrime [2..10000] []
+
+-- Exercise 5
+nextPrime :: Integer -> Integer
+nextPrime n = if prime n then n else nextPrime (n+1)
+
+consecutivePrime :: [Integer] -> Integer
+consecutivePrime p
+    | prime (sum p) = sum p
+    | otherwise = consecutivePrime ((drop 1 p) ++ [nextPrime (last p + 1)])
+
+test5 = consecutivePrime (take 101 primes)
+
+-- Exercise 6
+conjecturePrime :: [Integer] -> [Integer]
+conjecturePrime p
+    | prime ((product p) + 1) = conjecturePrime (p ++ [nextPrime (last p + 1)])
+    | otherwise = p
+
+test6 = conjecturePrime [2]
+
+-- Exercise 7
+--hoef niet in list kan meteen optellen, div en mod
+luhn :: Integer -> Bool
+luhn n = luhn2 (reversal n) 0 False
+
+luhn2 :: Integer -> Integer -> Bool -> Bool
+luhn2 0 c t
+    | c `mod` 10 == 0 = True
+    | otherwise = False
+luhn2 n c t
+    | t = luhn2 (n `div` 10) (c + luhn3 n) False
+    | otherwise = luhn2 (n `div` 10) (c + (n `mod` 10)) True
+
+luhn3 :: Integer -> Integer
+luhn3 n
+    | n > 10 = n - 9
+    | otherwise = n
+
+test7a = luhn 79927398713
+test7b = luhn 79927398714
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
