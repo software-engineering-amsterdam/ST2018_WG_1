d3a78c88e2d8e6077c5be617bead2f47d5ed427f
Lukas Koedijk <lukas@koedijk.net>
2018-09-13 10:07:01 +0200

    setup lab2

---
+++ lab2/sebastiaan/Lab2.hs
@@ -0,0 +1,24 @@
+
+module Lab2 where
+
+import Data.List
+import Data.Char
+import System.Random
+import Test.QuickCheck
+
+infix 1 --> 
+
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+probs :: Int -> IO [Float]
+probs 0 = return []
+probs n = do
+             p <- getStdRandom random
+             ps <- probs (n-1) 
+             return (p:ps)
+
+data Shape = NoTriangle | Equilateral 
+           | Isosceles  | Rectangular | Other deriving (Eq,Show)
+
+
