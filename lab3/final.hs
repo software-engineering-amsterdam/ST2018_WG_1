c90c6a2b3eebce0e95548e47102ed4ca5668c2a9
Remy <r.vd.vegt@hotmail.com>
2018-09-25 18:42:47 +0200

    added ex4 to final

--- lab3/final.hs
+++ lab3/final.hs
@@ -35,8 +35,7 @@ test1f = entails form2 form1
 
 -- | logical equivalence
 equiv :: Form -> Form -> Bool
-equiv f1 f2 = (filter (\ v -> evl v f1) (allVals f1)) ==
-(filter (\ v -> evl v f2) (allVals f2))
+equiv f1 f2 = (filter (\ v -> evl v f1) (allVals f1)) == (filter (\ v -> evl v f2) (allVals f2))
 
 test1g = equiv form1 form1
 test1h = equiv form1 form2
@@ -75,3 +74,113 @@ test3a = toCNF form3a
 test3b = toCNF form3b
 test3c = toCNF form3c
 test3d = toCNF form3d
+
+
+-- Exercise 4: (2:00)
+-- Node rules:
+-- - Prop:  Followed only by a Prop/Int.
+-- - Neg:   Followed by a subform.
+-- - Dsj:   Followed by a list of subforms.
+-- - Cnj:   Followed by a list of subforms.
+-- - Impl:  Followed by 2 separate subforms.
+-- - Equiv: Followed by 2 separate subforms.
+--
+-- There are no preconditions for the generator.
+-- There are postconditions though:
+-- - The output must be a valid formula.
+--   This is already checked in the generator, because the output
+--   has to be of type IO Form.
+-- - Furthermore could the generator fail if we do not create any Props.
+--   This is done by placing Props at every leaf in the generator.
+--   Also we defined a max depth, and at max depth we automatically place a Prop as leaf.
+
+getNodeType :: Int -> String
+getNodeType 0 = "Prop"
+getNodeType 1 = "Neg"
+getNodeType 2 = "Dsj"
+getNodeType 3 = "Cnj"
+getNodeType 4 = "Impl"
+getNodeType 5 = "Equiv"
+getNodeType _ = error "Non existing node type"
+
+randomNodeType :: [Int] -> IO String
+randomNodeType l = do
+    x <- randomRIO (0, (length l)-1)
+    return $ getNodeType $ l !! x
+
+-- Generate a random form.
+-- d is depth of the tree, if that is 0, than we create a prop instead of something else.
+-- This way we won't go infinitely deep at random.
+-- nt is a list of nodeTypes that are allowed to be used in the (sub)form.
+genForm :: Int -> [Int] -> IO Form
+genForm 0 _ = do
+    x <- randomRIO (1,5)
+    return $ Prop x
+genForm d nt = do
+    node        <- randomNodeType nt
+    prop        <- randomRIO (1,5)
+    f1          <- genForm (d-1) [0,1,2,3,4,5]
+    f2          <- genForm (d-1) [0,1,2,3,4,5]
+    x           <- randomRIO (2,5)
+    fn          <- genForms x (d-1) node
+    case node of
+        "Prop"  -> return $ Prop prop
+        "Neg"   -> return $ Neg f1
+        "Dsj"   -> return $ Dsj fn
+        "Cnj"   -> return $ Cnj fn
+        "Impl"  -> return $ Impl f1 f2
+        "Equiv" -> return $ Equiv f1 f2
+        _       -> error "Unknown node type"
+
+
+-- This generates a list of forms for conjunctions and disjunctions.
+genForms :: Int -> Int -> String -> IO [Form]
+genForms 0 _ _ = return []
+genForms x d n = do
+    f1 <- genForm d $ filter (\x -> getNodeType x /= n) [0,1,2,3,4,5]
+    fn <- genForms (x-1) d n
+    return $ f1:fn
+
+-- This function executes a function on a random generated formula.
+-- This works with the testParse function
+-- Also a depth should be given, so that we do not necessarily generate huge formulas.
+testParseRandom :: [Form] -> (Form -> Bool) -> IO ()
+testParseRandom [] _ = putStrLn "Passed test!"
+testParseRandom (f:fn) p = do
+    if (p f) then
+        do
+            testParseRandom fn p
+    else
+        putStrLn $ "Failed test:\n" ++ (show f)
+
+genNForms :: Int -> Int -> IO [Form]
+genNForms 0 _ = return []
+genNForms n d = do
+    f   <- genForm d [0,1,2,3,4,5]
+    fn  <- genNForms (n-1) d
+    return $ f:fn
+
+-- This function executes a function N times on random generated formulas.
+testParseRandomN :: Int -> Int -> (Form -> Bool) -> IO ()
+testParseRandomN 0 _ _ = putStrLn "Testing 0 times is not very useful, is it?"
+testParseRandomN n d p = do
+    ranForms <- genNForms n d
+    putStrLn $ "--- Start " ++ show n ++ " tests!"
+    testParseRandom ranForms p
+
+-- Test properties
+randTautology :: IO ()
+randTautology = testParseRandomN 100 3 tautology
+
+randContradiction :: IO ()
+randContradiction = testParseRandomN 100 3 contradiction
+
+randEquiv1, randEquiv2 :: IO ()
+randEquiv1 = testParseRandomN 100 3 (\x -> equiv x x)
+randEquiv2 = testParseRandomN 100 3 (\x -> equiv x $ Neg x)
+
+randCNF :: IO ()
+randCNF = testParseRandomN 100 3 (\x -> equiv (toCNF x) x)
+
+-- randParse :: IO ()
+-- randParse = testParseRandomN 100 3 testParse
