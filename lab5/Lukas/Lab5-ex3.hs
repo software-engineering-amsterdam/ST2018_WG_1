dd77375b62cfda45b0a6df34338bd7108faceacd
Lukas Koedijk <lukas@koedijk.net>
2018-10-08 22:01:30 +0200

    ex4

--- lab5/Lukas/Lab5-ex3.hs
+++ lab5/Lukas/Lab5-ex3.hs
@@ -362,3 +362,35 @@ minimal s = (uniqueSol s) &&
 subSudoku :: Node -> [Node]
 subSudoku (s, c) =  map (\x -> eraseN (s, c) x) (filledPositions s)
 
+-- Exercise 4 (2 hours)
+-- You can generate a sudoku with 4 empty grids, but not with 5.
+-- But only when the empty grid are not neighbour grids. This can
+-- be checked by removing grids, them minamizing the sudoku. Then
+-- we can check if it is possible by running our minimal check.
+eraseE :: Node -> [(Row, Column)] -> Node
+eraseE n [] = n
+eraseE n (x:xs) = eraseE (eraseN n x) xs
+
+-- Replaces grid b with empty grid.
+eraseB :: Node -> Integer -> Node
+eraseB n b = eraseE n bl
+    where
+        r = blocks !! (floor $ fromIntegral b / 3)
+        c = blocks !! (fromIntegral $ b `mod` 3)
+        bl = [(x,y) | x <- r, y <- c]
+
+eraseBs :: Node -> [Integer] -> Node
+eraseBs n [] = n
+eraseBs n (x:xs) = eraseBs (eraseB n x) xs
+
+-- This function removes the grids listed in x and then minimalize
+-- the sudoku. After that we check that this is the minimal solution.
+test4 :: [Integer] -> IO ()
+test4 x = do
+    s <- genRandomSudoku
+    showNode s
+    let sx = eraseBs s x
+    showNode sx
+    sy <- genProblem sx
+    showNode sy
+    print(minimal sy)
